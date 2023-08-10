a33a133e011e61552977d2e2c49d6ede4f346438
alessandramedeirosvieira <alessandra_m_vieira@estudante.sesisenai.org.br>
2023-08-09 22:30:57 -0300

    exemplo

--- exercicios-da-semana06/lista-tarefas/src/components/TaskComponents/Form/Formulario.jsx
+++ exercicios-da-semana06/lista-tarefas/src/components/TaskComponents/Form/Formulario.jsx
@@ -26,3 +26,20 @@ function Formulario({ handleSubmit }) {
 }
 
 export default Formulario
+
+//exemplo de formulário
+//const Form = () => {
+//  return (
+//   <div> 
+//       <form>
+//          <div>
+//              <label htmlFor="name">Nome:</label>
+//              <input type="text" name="name" placeholder="Digite a seu nome" />
+//           </div>
+//            input type="submit" value="Enviar" />
+//
+//        </form>
+//    </div>
+//)
+//}
+//}
